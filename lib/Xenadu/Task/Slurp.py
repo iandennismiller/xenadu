ca18999442ab038bfcf6348a2f93dfecaaf8029e
Ian Dennis Miller <ian@iandennismiller.com>
2011-04-30 15:18:35 -0400

    * added --get, to parallel --push * --get can use either local or remote filename * making sure --push will set permissions correctly on remote machine

--- lib/Xenadu/Task/Slurp.py
+++ lib/Xenadu/Task/Slurp.py
@@ -2,13 +2,21 @@ import Xenadu
 import os, shutil, subprocess, string, re, sys, logging
 
 def slurp_remote(remote_file, file_mapping=None):
+    entry = None
     try:
         entry = Xenadu.Env["Config"]["mapping"][remote_file]
     except:
+        for filename in Xenadu.Env["Config"]["mapping"]:
+            if Xenadu.Env["Config"]["mapping"][filename]['local_file'] == remote_file:
+                entry = Xenadu.Env["Config"]["mapping"][filename]
+
+    if not entry:
         logging.getLogger("Xenadu").error("can't find: %s" % remote_file)
         return
 
     local_file = os.path.join(Xenadu.Env["Config"]["guest_path"], "files", entry['local_file'])
+    remote_file = os.path.join(Xenadu.Env["Config"]["guest_path"], "files", entry['remote_file'])
+
     logging.getLogger("Xenadu").info("get remote file: %s" % remote_file)
     logging.getLogger("Xenadu").info("to local file: %s" % local_file)
     ssh = Xenadu.Env["Config"]["ssh"]
@@ -29,15 +37,22 @@ def push_remote(remote_file, file_mapping=None):
         logging.getLogger("Xenadu").error("can't find: %s" % remote_file)
         return
 
-    local_file = os.path.join(Xenadu.Env["Config"]["guest_path"], "files", entry['local_file'])
-    remote_file = os.path.join(Xenadu.Env["Config"]["guest_path"], "files", entry['remote_file'])
+    h = {
+        'local': os.path.join(Xenadu.Env["Config"]["guest_path"], "files", entry['local_file']),
+        'remote': os.path.join(Xenadu.Env["Config"]["guest_path"], "files", entry['remote_file']),
+        'user': Xenadu.Env["Config"]["ssh"]['user'],
+        'address': Xenadu.Env["Config"]["ssh"]['address'],
+        'owner': entry['owner'],
+        'group': entry['group'],
+        'perm': entry['perm'],
+    }
 
-    logging.getLogger("Xenadu").info("push local file: %s" % local_file)
-    logging.getLogger("Xenadu").info("to remote file: %s" % remote_file)
-    ssh = Xenadu.Env["Config"]["ssh"]
-    cmd = '/usr/bin/scp "%s" "%s@%s:%s"' % (local_file, ssh["user"], ssh["address"], remote_file)
+    logging.getLogger("Xenadu").info("push local file: %(local)s to remote file: %(remote)s" % h)
+    cmd = '/usr/bin/scp "%(local)s" "%(user)s@%(address)s:%(remote)s"' % h
     os.system(cmd)
-    #subprocess.Popen(["/usr/bin/scp", "%s@%s:%s" % (ssh["user"], ssh["address"], remote_file), local_file])
+    h['subcmd'] = 'chown %(owner)s:%(group)s "%(remote)s"; chmod %(perm)s "%(remote)s"' % h
+    cmd = "ssh %(user)s@%(address)s '%(subcmd)s'" % h
+    os.system(cmd)
 
 def slurp_local(filename, file_mapping = None):
     if file_mapping == None:
@@ -71,8 +86,9 @@ def diff(remote_file, file_mapping=None):
     os.system("diff -c /tmp/xenadu/diff.txt %s" % local_file)
 
 def register():
-    Xenadu.Env["Registry"].register_task(name="local", args=1, help="copy file from remote host based on local filename", function=slurp_local)
-    Xenadu.Env["Registry"].register_task(name="remote", args=1, help="copy file based on remote filename", function=slurp_remote)
+    #Xenadu.Env["Registry"].register_task(name="local", args=1, help="copy file from remote host based on local filename", function=slurp_local)
+    #Xenadu.Env["Registry"].register_task(name="remote", args=1, help="copy file based on remote filename", function=slurp_remote)
     Xenadu.Env["Registry"].register_task(name="getall", args=0, help="copy all files from remote host", function=slurp_all)
     Xenadu.Env["Registry"].register_task(name="diff", args=1, help="diff remote file with local version", function=diff)
     Xenadu.Env["Registry"].register_task(name="push", args=1, help="push local file to remote host", function=push_remote)
+    Xenadu.Env["Registry"].register_task(name="get", args=1, help="get a file from remote host", function=slurp_remote)
